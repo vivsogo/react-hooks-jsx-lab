bf610cee115e0477929f6f5e5b3423e1b2365f48
Vivian Gomez <viviangomez0@gmail.com>
2022-01-12 22:02:31 -0500

    completed

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div>About 
+  <div id="about">
+    <h2>About Me</h2>
+    <p>Hi I'm Viv</p>
+    <img src={image} alt="I made this"/>
+   </div>
+  </div>;
 }
 
 export default About;
